448d3558ee4aac38758ed987439ccd2852444343
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-10 22:59:39 +0300

    Add navigation bar to App component and include style.css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="row">
-      <div className="col-sm-4">.col-sm-4</div>
-      <div className="col-sm-4">.col-sm-4</div>
-      <div className="col-sm-4">.col-sm-4</div>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container">
+          {" "}
+          <a href="#" className="navbar-brand">
+            Frontend Bootcamp
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav  ms-auto">
+              <li className="nav_item">
+                <a href="#learn" className="nav-link">
+                  What you'll learn
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#questions" className="nav-link">
+                  Questions
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#instractors" className="nav-link">
+                  Instractors
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
