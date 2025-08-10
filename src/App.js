0d925d88f9d28a2174bfd63b9e9c4cea7ebcee07
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-11 02:12:38 +0300

    add instructor

--- src/App.js
+++ src/App.js
@@ -350,6 +350,134 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* Instructor */}
+      <section id="instructors" className="p-5 bg-primary">
+        <div className="container">
+          <h2 className="text-center text-white">Our Instructor</h2>
+          <p className="lead text-center text-white mb-5">
+            Our instructors all have 5+ years working as a web developer in the
+            industry.
+          </p>
+        </div>
+        <div className="row g-4">
+          <div className="col-md-6 col-lg-3">
+            <div className="card bg-light">
+              <div className="card-body text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/men/20.jpg"
+                  className="rounded-circle mb-3"
+                  alt=""
+                />
+                <h3 className="card-title mb-3">John Doe</h3>
+                <p className="card-text">
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                </p>
+                <a href="">
+                  <i className="bi bi-twitter text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-facebook text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-linkdin text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-instagram text-dark mx-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3">
+            <div className="card bg-light">
+              <div className="card-body text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/women/20.jpg"
+                  className="rounded-circle mb-3"
+                  alt=""
+                />
+                <h3 className="card-title mb-3">Jane Doe</h3>
+                <p className="card-text">
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                </p>
+                <a href="">
+                  <i className="bi bi-twitter text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-facebook text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-linkdin text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-instagram text-dark mx-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3">
+            <div className="card bg-light">
+              <div className="card-body text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/men/14.jpg"
+                  className="rounded-circle mb-3"
+                  alt=""
+                />
+                <h3 className="card-title mb-3">Steve Smith</h3>
+                <p className="card-text">
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                </p>
+                <a href="">
+                  <i className="bi bi-twitter text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-facebook text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-linkdin text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-instagram text-dark mx-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-3">
+            <div className="card bg-light">
+              <div className="card-body text-center">
+                <img
+                  src="https://randomuser.me/api/portraits/women/14.jpg"
+                  className="rounded-circle mb-3"
+                  alt=""
+                />
+                <h3 className="card-title mb-3">Sara Smith</h3>
+                <p className="card-text">
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                </p>
+                <a href="">
+                  <i className="bi bi-twitter text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-facebook text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-linkdin text-dark mx-1"></i>
+                </a>
+                <a href="">
+                  <i className="bi bi-instagram text-dark mx-1"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
